7c6bb5adbceabdb9b5ba8e344f221422010c70ac
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2022-02-15 10:57:43 -0600

    Using Comparable

--- src/main/java/org/_09_/collections/comparable/ComparableDemo.java
+++ src/main/java/org/_09_/collections/comparable/ComparableDemo.java
@@ -1,8 +1,12 @@
 package org._09_.collections.comparable;
-
 import java.util.TreeSet;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// Set don't duplicate elements
 public class ComparableDemo {
+	static final Logger log = LogManager.getLogger(ComparableDemo.class.getName());
 	public static void main(String[] args) {
 		TreeSet<Integer> data = new TreeSet<>();
 		data.add(152);
@@ -11,8 +15,22 @@ public class ComparableDemo {
 		data.add(10);
 		data.add(132);
 		data.add(10);
-		
 		System.out.println(data);
+		log.debug("data" + data);
 	}
 
 }
+
+/**
+ * 						-----------------152-----------------------
+ * 						|										   |
+ * 						10----							----------185--------
+ * 						     |							|					|
+ * 							132							154					254
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
