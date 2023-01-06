60f081674a137b28c861e8fb142fc6dc5364498a
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-01-06 16:04:27 -0600

    Default and Static Method Interface

---
+++ src/main/java/com/java/lambda/defaultstatic/interfaces/DefaultDemo.java
@@ -0,0 +1,59 @@
+package com.java.lambda.defaultstatic.interfaces;
+
+interface Vehicle{
+	String getBrand();
+	String speedUp();
+	String slowDown();
+	
+	default String turnAlarmOn() {
+		return "turning vehicle alarm on";
+	}
+	
+	default String turnAlarmOff() {
+		return "turning vehicle alarm off";
+	}
+
+	static String getCOmpany() {
+		return "BMW";
+	}
+}
+
+
+class Car implements Vehicle{
+
+	@Override
+	public String getBrand() {
+		return "BMW";
+	}
+
+	@Override
+	public String speedUp() {
+		return "the car is speeding up";
+	}
+
+	@Override
+	public String slowDown() {
+		return "the car is slowing down";
+	}
+	
+	
+}
+
+public class DefaultDemo {
+	public static void main(String[] args) {
+		Vehicle vehicle = new Car();
+		
+		System.out.println(vehicle.getBrand());
+		System.out.println(vehicle.speedUp());
+		System.out.println(vehicle.slowDown());
+		System.out.println(vehicle.turnAlarmOn());
+		System.out.println(vehicle.turnAlarmOff());
+		
+		
+		System.out.println(Vehicle.getCOmpany());
+		
+		
+		
+		
+	}
+}
