c07a737b0d0d09d79eebd514304d7d34b9ae194d
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-01-12 06:39:45 -0600

    Overriding and Overloading

---
+++ src/main/java/chapter_01/pag_004/oveloading/overriding/Bobcat.java
@@ -0,0 +1,5 @@
+package chapter_01.pag_004.oveloading.overriding;
+
+public class Bobcat {
+	public void findDen() {}
+}
---
+++ src/main/java/chapter_01/pag_004/oveloading/overriding/BobcatKitten.java
@@ -0,0 +1,7 @@
+package chapter_01.pag_004.oveloading.overriding;
+
+public class BobcatKitten extends Bobcat{
+	public void findDen() {} //overriding
+	public void finDen(boolean b) {} //overloading
+	public int findden() throws Exception{ return 0; } //diferent method
+}
