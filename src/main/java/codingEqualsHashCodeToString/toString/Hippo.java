7c97047f89df56447621f6ebd62ab75f77cee84f
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-01-16 22:16:10 -0600

    toString

---
+++ src/main/java/codingEqualsHashCodeToString/toString/Hippo.java
@@ -0,0 +1,23 @@
+package codingEqualsHashCodeToString.toString;
+
+public class Hippo {
+	private String name;
+	private double weight;
+	
+	public Hippo(String name, double weight) {
+		this.name = name;
+		this.weight = weight;
+	}
+	
+	@Override
+	public String toString() {
+		return "name: " + name + " weight: "+ weight;
+	}
+	
+	public static void main(String[] args) {
+		Hippo h1 = new Hippo("Harry", 3100);
+		System.out.println(h1);
+	}
+	
+	
+}
