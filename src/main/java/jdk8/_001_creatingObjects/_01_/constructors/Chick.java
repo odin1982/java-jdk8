738598cd97c9f52c8bc8c116bdd8c3b7388547b3
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-01-16 21:07:36 -0600

    constructor

---
+++ src/main/java/jdk8/_001_creatingObjects/_01_/constructors/Chick.java
@@ -0,0 +1,7 @@
+package jdk8._001_creatingObjects._01_.constructors;
+
+public class Chick {
+	public Chick() {
+		System.out.println("In constructor");
+	}
+}
---
+++ src/main/java/jdk8/_001_creatingObjects/_01_/constructors/Chicken.java
@@ -0,0 +1,11 @@
+package jdk8._001_creatingObjects._01_.constructors;
+
+public class Chicken {
+	int numEgss = 0;
+	String name;
+	
+	public Chicken() {
+		name = "Ducke";
+	}
+
+}
