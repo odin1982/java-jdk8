9775e30f134cc6310f64e02cb803cb1688de00d9
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-01-17 22:29:10 -0600

    reading and writing objects

---
+++ src/main/java/jdk8/_001_/readingAndWritingObjectFields/Name.java
@@ -0,0 +1,7 @@
+package jdk8._001_.readingAndWritingObjectFields;
+
+public class Name {
+	String first = "Theodore";
+	String last = "Moose";
+	String full = first +  last;
+}
---
+++ src/main/java/jdk8/_001_/readingAndWritingObjectFields/Swan.java
@@ -0,0 +1,11 @@
+package jdk8._001_.readingAndWritingObjectFields;
+
+public class Swan {
+	int numberEggs;
+	public static void main(String[] args) {
+		Swan mother = new Swan();
+		mother.numberEggs = 1;
+		System.out.println("mother.numberEggs: " + mother.numberEggs);
+	}
+
+}
