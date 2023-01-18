1a732bdc841bf3d4940597a827ab863efe01577a
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-01-18 05:31:23 -0600

    order of initialization

---
+++ src/main/java/jdk8/_001_/instanceInitializerBlocks/BlocsExample.java
@@ -0,0 +1,10 @@
+package jdk8._001_.instanceInitializerBlocks;
+
+public class BlocsExample {
+	public static void main(String[] args) {
+		{
+			System.out.println("Feathers");
+		}
+	}
+	{System.out.println("Snowy");}
+}
---
+++ src/main/java/jdk8/_001_/instanceInitializerBlocks/Chick.java
@@ -0,0 +1,16 @@
+package jdk8._001_.instanceInitializerBlocks;
+
+public class Chick {
+	private String name = "Fluffy";
+	{System.out.println("setting field");}
+	
+	public Chick() {
+		name="Tiny";
+		System.out.println("setting constructor");
+	}
+	
+	public static void main(String[] args) {
+		Chick chick = new Chick();
+		System.out.println(chick.name);
+	}
+}
