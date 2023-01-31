5e83261ecad482287be8f7aa8ec4c4df2e3d3ce5
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-01-30 21:27:45 -0600

    implementing functional interfaces with Lambdas

---
+++ src/main/java/chapter_02/designPatternsAndPrinciples/_02/introducingFunctionalPrograming/_02/implementingFunctionalInterfacesWithLambdas/CheckTrait.java
@@ -0,0 +1,5 @@
+package chapter_02.designPatternsAndPrinciples._02.introducingFunctionalPrograming._02.implementingFunctionalInterfacesWithLambdas;
+
+public interface CheckTrait {
+	public boolean test(Animal a);
+}
