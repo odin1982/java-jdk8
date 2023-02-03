0e59ab016445eb0540831232ecab626c68e54126
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-02-03 12:36:06 -0600

    applying lazy instantiation to Singletons

---
+++ src/main/java/chapter_02/_04/workingWithDesignPatterns/_01/applyingLazyInstantiationToSingleton/VisitorTicketTracker.java
@@ -0,0 +1,12 @@
+package chapter_02._04.workingWithDesignPatterns._01.applyingLazyInstantiationToSingleton;
+
+public class VisitorTicketTracker {
+	private static VisitorTicketTracker instance;
+	private VisitorTicketTracker() {}
+	public static VisitorTicketTracker getInstance() {
+		if(instance == null) {
+			instance = new VisitorTicketTracker();
+		}
+		return instance;
+	}
+}
