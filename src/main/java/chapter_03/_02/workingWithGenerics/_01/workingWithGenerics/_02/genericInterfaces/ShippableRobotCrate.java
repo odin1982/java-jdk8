85aa1ccb393a9744148042e3aeceb10e8730a779
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-02-16 07:36:26 -0600

    rename file

--- src/main/java/chapter_03/_02/workingWithGenerics/_01/workingWithGenerics/_02/genericInterfaces/ShippableRobotCrate.java
+++ src/main/java/chapter_03/_02/workingWithGenerics/_01/workingWithGenerics/_02/genericInterfaces/ShippableRobotCrate.java
@@ -1,6 +1,6 @@
 package chapter_03._02.workingWithGenerics._01.workingWithGenerics._02.genericInterfaces;
 
-public class SgippableRobotCrate implements Shippable<Robot>{
+public class ShippableRobotCrate implements Shippable<Robot>{
 
 	@Override
 	public void ship(Robot t) {
