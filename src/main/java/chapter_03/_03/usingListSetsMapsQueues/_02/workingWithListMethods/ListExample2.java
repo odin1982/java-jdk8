9ec621c1ee59f99d9d0c17812b15bfbe1856b5b6
odinLaptopUbuntu <odin.araujo.barragan@gmail.com>
2023-03-09 17:30:57 -0600

    List Collection

---
+++ src/main/java/chapter_03/_03/usingListSetsMapsQueues/_02/workingWithListMethods/ListExample2.java
@@ -0,0 +1,14 @@
+package chapter_03._03.usingListSetsMapsQueues._02.workingWithListMethods;
+
+import java.util.ArrayList;
+
+public class ListExample2 {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("OH");
+		list.add("CO");
+		list.add("NJ");
+		String state = list.get(0);
+		int index = list.indexOf("NJ");
+	}
+}
