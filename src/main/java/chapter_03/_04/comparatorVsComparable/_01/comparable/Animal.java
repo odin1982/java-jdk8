45b50cb4b4ee1e54e953ed6f5ab06ea41a8a2370
odinLaptopUbuntu <odin.araujo.barragan@gmail.com>
2023-03-23 21:35:59 -0600

    Comparable

---
+++ src/main/java/chapter_03/_04/comparatorVsComparable/_01/comparable/Animal.java
@@ -0,0 +1,20 @@
+package chapter_03._04.comparatorVsComparable._01.comparable;
+
+public class Animal implements Comparable<Animal>{
+	private int id;
+
+	@Override
+	public int compareTo(Animal a) {
+		return id - a.id;
+	}
+	
+	public static void main(String[] args) {
+		Animal a1 = new Animal();
+		Animal a2 = new Animal();
+		a1.id = 5;
+		a2.id = 7;
+		System.out.println(a1.compareTo(a2));
+		System.out.println(a1.compareTo(a1));
+		System.out.println(a2.compareTo(a1));
+	}
+}
