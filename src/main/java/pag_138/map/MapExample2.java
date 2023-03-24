4e6581102b66d558925223e6f3c3d014b3ec4870
odinLaptopUbuntu <odin.araujo.barragan@gmail.com>
2023-03-23 20:26:44 -0600

    Map

--- src/main/java/pag_138/map/MapExample2.java
+++ src/main/java/pag_138/map/MapExample2.java
@@ -1,17 +1,18 @@
-package pag138.map;
+package pag_138.map;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
-public class MapExample {
+public class MapExample2 {
 	public static void main(String[] args) {
-		Map<String,String> map = new HashMap<>();
-		map.put("koala", "bamboo");
-		map.put("lion", "meat");
-		map.put("giraffe", "leaf");
+		Map<String,String> map = new TreeMap<>();
+		map.put("koala","bamboo");
+		map.put("lion","meat");
+		map.put("giraffe","leaf");
 		String food = map.get("koala");
 		System.out.println("food: " + food);
 		for(String key: map.keySet())
 			System.out.print(key + ",");
 	}
+
 }
