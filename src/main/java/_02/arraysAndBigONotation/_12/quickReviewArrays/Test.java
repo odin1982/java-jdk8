2c8579f3478ee4e7ea8e088bf898b0d52eb56087
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-06-09 22:13:10 -0600

    arrays and Big-O notation -quick review of Arrays in Java

---
+++ src/main/java/_02/arraysAndBigONotation/_12/quickReviewArrays/Test.java
@@ -0,0 +1,19 @@
+package _02.arraysAndBigONotation._12.quickReviewArrays;
+
+public class Test {
+	public static void main(String[] args) {
+		int[] intArray = new int[7];	//you need to specify de size
+		intArray[0] = 20;
+		intArray[1] = 35;
+		intArray[2] = -15;
+		intArray[3] = 7;
+		intArray[4] = 55;
+		intArray[5] = 1;
+		intArray[6] = -22;
+		
+		for(int i=0;i<intArray.length;i++) {
+			System.out.println(intArray[i]);
+		}
+		
+	}
+}
