3db041d916ee0cbd54217b502f541bbbb0aa9415
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-09-04 22:04:06 -0500

    Returning an optional

---
+++ src/main/java/chapter_04/_03/returningAnOptional/OptionalSample.java
@@ -0,0 +1,41 @@
+package chapter_04._03.returningAnOptional;
+
+import java.util.Optional;
+
+public class OptionalSample {
+	public static Optional<Double> average(int... scores){
+		if(scores.length == 0) {
+			return Optional.empty();
+		}
+		int sum = 0;
+		for(int score:scores) {
+			sum += score;
+		}
+		return Optional.of(((double)sum)/scores.length);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(average(1,2,3));
+		System.out.println(average(1,2,3).get());
+		
+		Optional<Double> opt = average(90,100);
+		if(opt.isPresent()) {
+			System.out.println(opt.get());
+		}
+		
+//		Optional<Double> opt2 = average();
+//		System.out.println(opt2.get());
+		
+		System.out.println(Optional.ofNullable(null));
+		
+		Optional<Double> o = average(90,100);
+		o.ifPresent(value -> System.out.println(value));
+		o.ifPresent(System.out::println);
+		
+		Optional<Double> opti = average();
+		System.out.println(opti.orElse(Double.NaN));
+		System.out.println(opti.orElseGet(()->Math.random()));
+		System.out.println(opt.orElseThrow(() -> new IllegalStateException()));
+		
+	}
+}
