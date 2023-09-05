fbfdde6c7a2eadbcfa80683b60eadf42a726f9f3
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2023-09-04 22:34:16 -0500

    Creating stream sources

---
+++ src/main/java/chapter_04/_04/usingStreams/_01/creatingStreamSources/StreamsSample.java
@@ -0,0 +1,18 @@
+package chapter_04._04.usingStreams._01.creatingStreamSources;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamsSample {
+	public static void main(String[] args) {
+		Stream<String> empty = Stream.empty();
+		Stream<Integer> singleElement = Stream.of(1);
+		Stream<Integer> fromArray = Stream.of(1,2,3);
+		List<String> list = Arrays.asList("a","b","c");
+		Stream<String> fromList = list.stream();
+		Stream<String> fromListParallel = list.parallelStream();
+		Stream<Double> randoms = Stream.generate(Math::random);
+		Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);
+	}
+}
