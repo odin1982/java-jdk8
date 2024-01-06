919ad31e775f416a9d7f895ec3e70b2dadd93d8f
odin-huawei-pc <odin.araujo.barragan@gmail.com>
2024-01-06 13:05:14 -0600

    Concurrency - Introducing Threads - Creating a Thread

--- src/main/java/chapter_07/concurrency/_01/introducingThreads/_04/CreatingAThread/Test.java
+++ src/main/java/chapter_07/concurrency/_01/introducingThreads/_04/CreatingAThread/Test.java
@@ -1,8 +1,18 @@
 package chapter_07.concurrency._01.introducingThreads._04.CreatingAThread;
 
+import static org.hamcrest.CoreMatchers.startsWith;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("begin");
+        (new ReadInventoryThread()).start();
+        (new Thread(new PrintData())).start();
+        (new ReadInventoryThread()).start();
+        (new ReadInventoryThread()).start();
+        (new Thread(new PrintData())).start();
+        (new ReadInventoryThread()).start();
+        (new ReadInventoryThread()).start();
+        (new Thread(new PrintData())).start();
         (new ReadInventoryThread()).start();
     }
 }
