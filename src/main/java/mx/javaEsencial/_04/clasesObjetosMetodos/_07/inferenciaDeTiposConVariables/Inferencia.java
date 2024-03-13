8ffbd537ae44e0f4be8088fa409d2d698fbd1f98
odin <odin.araujo.barragan@gmail.com>
2024-03-12 20:59:39 -0600

    clasesObjetosMetodos - inferenciaDeTiposConVariables

---
+++ src/main/java/mx/javaEsencial/_04/clasesObjetosMetodos/_07/inferenciaDeTiposConVariables/Inferencia.java
@@ -0,0 +1,10 @@
+package mx.javaEsencial._04.clasesObjetosMetodos._07.inferenciaDeTiposConVariables;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		var numero = 3.3;
+		var palabra = "casa";
+		
+		System.out.println("La variable palabra es de tipo: " + palabra.getClass().getSimpleName());
+	}
+}
