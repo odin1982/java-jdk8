f0b5e00c2057a05d6439f110e23be425dc642917
odin <odin.araujo.barragan@gmail.com>
2024-03-15 18:52:57 -0600

    otrosTiposDeDatosImportantesEnJava - arraysMultidimensionales

---
+++ src/main/java/mx/javaEsencial/_05/otrosTiposDeDatosImportantesEnJava/_02/arraysMultidimensionales/ArraysMultidimensionales.java
@@ -0,0 +1,7 @@
+package mx.javaEsencial._05.otrosTiposDeDatosImportantesEnJava._02.arraysMultidimensionales;
+
+public class ArraysMultidimensionales {
+	public static void main(String[] args) {
+		int miArrayDeUnaDimension[] = new int[20];
+	}
+}
