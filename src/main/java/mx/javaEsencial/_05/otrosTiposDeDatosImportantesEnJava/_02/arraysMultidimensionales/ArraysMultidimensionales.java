fa42fc1575163c42da5a83cc5add7c0b09eea288
odin <odin.araujo.barragan@gmail.com>
2024-03-16 13:54:13 -0600

    otrosTiposDeDatosImportantesEnJava - arraysMultidimensionales

--- src/main/java/mx/javaEsencial/_05/otrosTiposDeDatosImportantesEnJava/_02/arraysMultidimensionales/ArraysMultidimensionales.java
+++ src/main/java/mx/javaEsencial/_05/otrosTiposDeDatosImportantesEnJava/_02/arraysMultidimensionales/ArraysMultidimensionales.java
@@ -3,5 +3,19 @@ package mx.javaEsencial._05.otrosTiposDeDatosImportantesEnJava._02.arraysMultidi
 public class ArraysMultidimensionales {
 	public static void main(String[] args) {
 		int miArrayDeUnaDimension[] = new int[20];
+		int miArrayDeDosDimensiones[][] = new int[10][20];
+		int miArrayDeTresDimensiones[][][] = new int[5][15][8];
+		
+		int tablaDeMultiplicacion[][] = new int[10][10];
+		
+		for(int i=0; i<10;i++) {
+			for(int j=0; j<10;j++) {
+				tablaDeMultiplicacion[i][j] = i*j;
+			}
+		}
+		
+		System.out.println("El resultado de multiplicar 4 por 5 esta en la posicion [4][5]: " + tablaDeMultiplicacion[4][5]);
+		
+		int miArrayMultidimensional[][] = {{2,5,-9},{7,25,0},{8,3,4},{5,-6,85,69,287}};
 	}
 }
